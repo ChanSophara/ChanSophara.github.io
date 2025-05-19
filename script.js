eeb0bc7bc8d40e8ab0652f32f1bff7dc8eca1fb4
Sophara <Sopharachan90@gmail.com>
2025-05-20 02:41:29 +0700

    Update: Update mobile menu to portfolio

--- script.js
+++ script.js
@@ -19,14 +19,14 @@ document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.querySelector('.hamburger');
     const navLinks = document.querySelector('nav ul');
     const mobileMenuOverlay = document.querySelector('.mobile-menu-overlay');
-    
+
     hamburger.addEventListener('click', function() {
         this.classList.toggle('active');
         navLinks.classList.toggle('active');
         mobileMenuOverlay.classList.toggle('active');
         document.body.style.overflow = navLinks.classList.contains('active') ? 'hidden' : 'auto';
     });
-    
+
     // Close mobile menu when clicking a link or overlay
     document.querySelectorAll('nav ul li a').forEach(link => {
         link.addEventListener('click', () => {
@@ -36,14 +36,13 @@ document.addEventListener('DOMContentLoaded', function() {
             document.body.style.overflow = 'auto';
         });
     });
-    
+
     mobileMenuOverlay.addEventListener('click', () => {
         hamburger.classList.remove('active');
         navLinks.classList.remove('active');
         mobileMenuOverlay.classList.remove('active');
         document.body.style.overflow = 'auto';
     });
-
     // Sticky Header
     window.addEventListener('scroll', function() {
         const header = document.querySelector('header');
